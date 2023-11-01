24569f421c1f6ebca2fad5c9cab63c6f245ead6b
Nemtrack <balazsgyenes10@gmail.com>
2023-11-01 22:30:33 +0100

    Add import to fix issue with former application showal

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,10 +6,11 @@ import { AppComponent } from './app.component';
 import { SimplePredictionComponent } from './simple-prediction/simple-prediction.component';
 import { FormsModule } from '@angular/forms';
 import { CardComponent } from './simple-prediction/card/card.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent, SimplePredictionComponent, CardComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
