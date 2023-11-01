262fcfd865bc9f6b1f0fe2f57b47e206b34613bc
Nemtrack <balazsgyenes10@gmail.com>
2023-11-01 22:33:08 +0100

    Make former weather application routable

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SimplePredictionComponent } from './simple-prediction/simple-prediction.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'weather', component: SimplePredictionComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
