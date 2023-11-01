7ac7db9b63a15f6a211b477812d242c7df81902e
Nemtrack <balazsgyenes10@gmail.com>
2023-11-01 22:59:25 +0100

    Configure route for first application startup and /welcome

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SimplePredictionComponent } from './simple-prediction/simple-prediction.component';
+import { WelcomeScreenComponent } from './welcome-screen/welcome-screen.component';
 
 const routes: Routes = [
+  { path: '', redirectTo: '/welcome', pathMatch: 'full' },
+  { path: 'welcome', component: WelcomeScreenComponent },
   { path: 'weather', component: SimplePredictionComponent },
 ];
 
