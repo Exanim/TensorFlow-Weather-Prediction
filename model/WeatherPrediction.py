620233816715dd71367530ea4e18d9ba23d1ab78
Exanim <simonbarath7@gmail.com>
2023-11-03 16:21:14 +0100

    Add multithreading to predict_weather

--- model/WeatherPrediction.py
+++ model/WeatherPrediction.py
@@ -1,4 +1,5 @@
 import os
+import asyncio
 import datetime
 import numpy as np
 import pandas as pd
@@ -20,21 +21,21 @@ class WeatherPrediction:
         self.model_path = f'models/year_ignore_{city}_model.keras'
         self.loaded_model = tf.keras.models.load_model(self.model_path)
 
-    def predict_weather(self, start_date, end_date):
-        current_date = start_date
-        delta = datetime.timedelta(days=1)
+
+    async def predict_weather_async(self, start_date, end_date):
         pred_list = []
 
+        current_date = start_date
+        delta = datetime.timedelta(days=1)
+        tasks = []
+
         while current_date <= end_date:
-            new_data = np.array([[current_date.month, current_date.day]])
-            predictions = self.scaler.inverse_transform(self.loaded_model.predict(new_data))
-
-            temperature = logarithmic_warming_rate(current_date.year, predictions[0, 0])
-            rain = chaotic_randomizer.chaotic_random()
-
-            pred_list.append([temperature, rain])
-
+            tasks.append(self.process_date(current_date))
             current_date += delta
+
+        results = await asyncio.gather(*tasks)
+        pred_list.extend(results)
+
         return pred_list
 
 
@@ -44,4 +45,9 @@ if __name__ == "__main__":
     weather_predictor = WeatherPrediction(city)
     start_date = datetime.date(2500, 11, 1)
     end_date = datetime.date(2502, 1, 1)
-    print(weather_predictor.predict_weather(start_date, end_date))
+
+    # Use async event loop
+    loop = asyncio.get_event_loop()
+    prediction_list = loop.run_until_complete(weather_predictor.predict_weather_async(start_date, end_date))
+    loop.close()
+    print(prediction_list)
