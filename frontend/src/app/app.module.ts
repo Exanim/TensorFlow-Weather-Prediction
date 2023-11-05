fa7a981e9d00cdf7f749fedf31efc20062d10420
Nemtrack <balazsgyenes10@gmail.com>
2023-11-05 14:41:59 +0100

    Create cities component in advanced-prediction

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -8,9 +8,10 @@ import { FormsModule } from '@angular/forms';
 import { CardComponent } from './simple-prediction/card/card.component';
 import { HttpClientModule } from '@angular/common/http';
 import { WelcomeScreenComponent } from './welcome-screen/welcome-screen.component';
+import { CitiesComponent } from './advanced-prediction/cities/cities.component';
 
 @NgModule({
-  declarations: [AppComponent, SimplePredictionComponent, CardComponent, WelcomeScreenComponent],
+  declarations: [AppComponent, SimplePredictionComponent, CardComponent, WelcomeScreenComponent, CitiesComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
