833bb1a84b8da0a461f8176481b4e90cddfc77eb
Barath Simon (Contractor) <simon.barath@capgemini.com>
2023-11-13 15:34:11 +0100

    Add helper function to replace missing values

---
+++ model/csv_cleaner.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+
+def replace_hiány_with_mean(file_path):
+    df = pd.read_csv(file_path)
+    df.replace("hiány", pd.NA, inplace=True)
+    df = df.apply(pd.to_numeric, errors='ignore')
+    df.fillna(df.mean(), inplace=True)
+
+    return df
