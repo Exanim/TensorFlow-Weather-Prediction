89879fb33a68d553204545b90cd9d0eb302d210d
Deák Erik <thferik@protonmail.com>
2023-11-13 13:03:33 +0100

    Import get-all-request.model.ts for weather

---
+++ frontend/src/app/advanced-prediction/weather/shared/get-all-request.model.ts
@@ -0,0 +1,69 @@
+export interface WeatherAPIResponse {
+  coord: Coord;
+  weather: Weather[];
+  base: string;
+  main: Main;
+  visibility: number;
+  wind: Wind;
+  rain: Rain;
+  clouds: Clouds;
+  dt: number;
+  sys: Sys;
+  timezone: number;
+  id: number;
+  name: string;
+  cod: number;
+}
+
+export interface GeoAPIResponse {
+  name: string;
+  local_names: string[];
+  lat: number;
+  lon: number;
+  country: string;
+}
+
+export interface Coord {
+  lon: number;
+  lat: number;
+}
+
+export interface Weather {
+  id: number;
+  main: string;
+  description: string;
+  icon: string;
+}
+
+export interface Main {
+  temp: number;
+  feels_like: number;
+  temp_min: number;
+  temp_max: number;
+  pressure: number;
+  humidity: number;
+  sea_level: number;
+  grnd_level: number;
+}
+
+export interface Wind {
+  speed: number;
+  deg: number;
+  gust: number;
+}
+
+export interface Rain {
+  '1h': number;
+}
+
+export interface Clouds {
+  all: number;
+}
+
+export interface Sys {
+  type: number;
+  id: number;
+  country: string;
+  sunrise: number;
+  sunset: number;
+}
