20831ca5ca926b6bc773c6a8867cc87cde336551
Deák Erik <thferik@protonmail.com>
2023-11-13 13:06:57 +0100

    Update app-routing.module.ts for city-selection and weather pages

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SimplePredictionComponent } from './simple-prediction/simple-prediction.component';
 import { WelcomeScreenComponent } from './welcome-screen/welcome-screen.component';
+import { CitySelectionComponent } from './advanced-prediction/city-selection/city-selection.component';
+import { WeatherComponent } from './advanced-prediction//weather/weather.component';
 import { CitiesComponent } from './advanced-prediction/cities/cities.component';
 
 const routes: Routes = [
@@ -9,6 +11,9 @@ const routes: Routes = [
   { path: 'welcome', component: WelcomeScreenComponent },
   { path: 'weather', component: SimplePredictionComponent },
   { path: 'tensor', component: CitiesComponent},
+  { path: 'city-selection', component: CitySelectionComponent },
+  { path: 'weather/:city', component: WeatherComponent },
+  { path: '', redirectTo: '/city-selection', pathMatch: 'full' },
 ];
 
 @NgModule({
