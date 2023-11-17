e70685809a3e918ffb0cd1900f974fbb0f70888c
bmart71 <bmart71@mailbox.unideb.hu>
2023-11-17 11:15:57 +0100

    Implement dummy parameter in do_GET

--- weather-api/api.py
+++ weather-api/api.py
@@ -1,6 +1,7 @@
 import json
 import logging
 from http.server import HTTPServer, BaseHTTPRequestHandler
+from urllib.parse import urlparse, parse_qs
 
 hostname = "localhost"
 port = 8000
@@ -13,7 +14,16 @@ bar = 0.02
 
 class WeatherApi(BaseHTTPRequestHandler):
     def do_GET(self):
+        global foo
         logging.info("GET STARTED")
+
+        parsed_path = urlparse(self.path)
+        query_params = parse_qs(parsed_path.query)
+
+        if not query_params.get('param1',[None])[0] == None:
+            foo = query_params.get('param1',[None])[0]
+
+
         self.send_response(200)
         self.send_header("Content-type", "application/json")
         self.end_headers()
