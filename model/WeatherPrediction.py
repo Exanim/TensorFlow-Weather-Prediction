f1c638ed2b8feba1a4a7b4778016c62440973f14
bmart71 <bmart71@mailbox.unideb.hu>
2023-11-22 14:34:22 +0100

    Connect http server with Ai model

--- model/WeatherPrediction.py
+++ model/WeatherPrediction.py
@@ -1,6 +1,11 @@
+import json
+import logging
 import os
 import asyncio
 import datetime
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from urllib.parse import urlparse, parse_qs
+
 import csv_cleaner
 import numpy as np
 import pandas as pd
@@ -8,6 +13,12 @@ import tensorflow as tf
 from sklearn.preprocessing import StandardScaler
 from output_transform import chaotic_randomizer
 from output_transform.global_warming_rate import logarithmic_warming_rate
+
+
+hostname = "localhost"
+port = 8000
+
+title = "TensorFlow-Weather-API"
 
 
 class WeatherPrediction:
@@ -47,15 +58,58 @@ class WeatherPrediction:
         return pred_list
 
 
+async def get_weather_data():
+    weather_predictor = WeatherPrediction("Budapest")
+    task = asyncio.create_task(weather_predictor.process_date(datetime.date.today()))
+    return await task
+
+
+class WeatherApi(BaseHTTPRequestHandler):
+    def do_GET(self):
+        logging.info("GET STARTED")
+
+        data = asyncio.run(get_weather_data())
+        print(data)
+
+        parsed_path = urlparse(self.path)
+        query_params = parse_qs(parsed_path.query)
+
+        if not query_params.get('param1',[None])[0] is None:
+            foo = query_params.get('param1',[None])[0]
+
+        self.send_response(200)
+        self.send_header("Content-type", "application/json")
+        self.end_headers()
+
+        response_data = {
+            "temperature": data[0],
+            "humidity": data[1],
+        }
+
+        response_json = json.dumps(response_data)
+        self.wfile.write(bytes(response_json, "utf-8"))
+
+
 # Example usage:
 if __name__ == "__main__":
-    city = "Budapest"
-    weather_predictor = WeatherPrediction(city)
-    start_date = datetime.date(2500, 1, 1)
-    end_date = datetime.date(2500, 2, 1)
+    # city = "Budapest"
+    # weather_predictor = WeatherPrediction(city)
+    # start_date = datetime.date(2500, 1, 1)
+    # end_date = datetime.date(2500, 1, 1)
+    #
+    # # Use async event loop
+    # loop = asyncio.get_event_loop()
+    # prediction_list = loop.run_until_complete(weather_predictor.predict_weather_async(start_date, end_date))
+    # loop.close()
+    # print(prediction_list)
+    webServer = HTTPServer((hostname, port), WeatherApi)
+    print("Server started http://%s:%s" % (hostname, port))
 
-    # Use async event loop
-    loop = asyncio.get_event_loop()
-    prediction_list = loop.run_until_complete(weather_predictor.predict_weather_async(start_date, end_date))
-    loop.close()
-    print(prediction_list)
+    try:
+        webServer.serve_forever()
+    except KeyboardInterrupt:
+        pass
+
+    webServer.server_close()
+    print("Server stopped.")
+    # print(asyncio.run(get_weather_data()))
