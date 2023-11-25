e0181b11409928cadf7cabd10e54ac1ff052801e
Balázs Gyenes <balazsgyenes10@gmail.com>
2023-11-25 17:17:33 +0100

    Output data correctly finished

--- frontend/src/app/advanced-prediction/predicted-city/day/day.component.ts
+++ frontend/src/app/advanced-prediction/predicted-city/day/day.component.ts
@@ -7,6 +7,8 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class DayComponent implements OnInit {
   @Input() day?: string;
+  @Input() temperature?: number;
+  @Input() humidity?: number;
   constructor() {}
   ngOnInit(): void {}
 }
