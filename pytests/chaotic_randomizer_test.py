9c6fc0f77ed2ee62fc77e78e093cd172732c0997
Exanim <simonbarath7@gmail.com>
2023-11-27 10:53:52 +0100

    Add tests for chaotic_randomizer_test.py

---
+++ pytests/chaotic_randomizer_test.py
@@ -0,0 +1,29 @@
+import unittest
+import random
+from model.output_transform.chaotic_randomizer import chaotic_random
+
+
+class TestChaoticRandom(unittest.TestCase):
+    def test_output_type(self):
+        result = chaotic_random()
+        self.assertTrue(isinstance(result, (float, int)), "Output should be of type float or int")
+
+    def test_output_range(self):
+        result = chaotic_random()
+        self.assertTrue(0.0 <= result <= 25.0, "Output should be in the range [0.0, 25.0]")
+
+    def test_upper_bound(self):
+        # Test upper bound to ensure that the function can handle repeated calls without errors
+        for _ in range(1000):
+            result = chaotic_random()
+            self.assertTrue(0.0 <= result <= 25.0, "Output should be in the range [0.0, 25.0]")
+
+    def test_lower_bound(self):
+        # Test lower bound to ensure that the function can handle repeated calls without errors
+        for _ in range(1000):
+            result = chaotic_random()
+            self.assertTrue(isinstance(result, (float, int)), "Output should be of type float or int")
+
+
+if __name__ == '__main__':
+    unittest.main()
